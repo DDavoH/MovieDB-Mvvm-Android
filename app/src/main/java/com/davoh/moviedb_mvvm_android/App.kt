a4f41e7db56f5b9caf2adf4a8141956177fed57b
David Cruz Ramirez <davoh.dev@gmail.com>
2022-01-08 09:41:29 -0600

    dependency injection implemented

---
+++ app/src/main/java/com/davoh/moviedb_mvvm_android/App.kt
@@ -0,0 +1,7 @@
+package com.davoh.moviedb_mvvm_android
+
+import dagger.hilt.android.HiltAndroidApp
+import android.app.Application
+
+@HiltAndroidApp
+class App : Application() {}
--- app/src/main/java/com/davoh/moviedb_mvvm_android/MainActivity.kt
+++ app/src/main/java/com/davoh/moviedb_mvvm_android/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.moviedb_mvvm_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.davoh.moviedb_mvvm_android.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
