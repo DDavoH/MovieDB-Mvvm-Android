9024f316525ada16739ddb7908e7d723fec949a9
David Cruz Ramirez <davoh.dev@gmail.com>
2022-01-08 08:47:53 -0600

    first commit

---
+++ app/src/main/java/com/davoh/moviedb_mvvm_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviedb_mvvm_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davoh.moviedb_mvvm_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
